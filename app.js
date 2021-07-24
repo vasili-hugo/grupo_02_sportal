984ce13a7f51ab7761efb345f2ef1cd10debfd79
slperin <perinsl@gmail.com>
2021-07-24 17:53:43 -0300

    Mobile home page version 0.0.1

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require ('path');
 
 const publicPath = path.join (__dirname, '/public');
-app.use ( express.static(publicPath));
-
-app.listen (8000, () => {
-    console.log ('Servidor corriendo correctamente en el url http://localhost:8000')
-})
+app.use (express.static(publicPath));
 
 app.get ('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'));
+})
+
+app.listen (8000, () => {
+    console.log ('Servidor corriendo en http://localhost:8000')
 })
